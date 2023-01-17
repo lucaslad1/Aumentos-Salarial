5b20579c4ba670dcb5b2122c2764fd2b92047375
lucaslad1 <lucasdaniel0904@gmail.com>
2023-01-17 15:09:41 -0300

    fiz mudanças nos comandos

---
+++ sistema/dados.py
@@ -0,0 +1,95 @@
+salarioinicial = 1000
+aumento = 0.015
+Ano = int(input("Digite o ano em que você quer ver o valor do salário: "))
+reajuste = float
+salario = float
+salario1 = float
+salario2 = float
+
+if Ano == 2000:
+    print("O salário será de: ", salarioinicial)
+
+if Ano == 2001:
+    reajuste = salarioinicial * aumento
+    salario1 = salarioinicial + reajuste
+    print("O salário será de: ",salario1)
+
+if Ano == 2002:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste
+    print("O salário será de: ",salario2)
+
+if Ano == 2003:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2004:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2005:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2006:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2007:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2008:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2009:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2010:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2011:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2012:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2013:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2014:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2015:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2016:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
+
+if Ano == 2017:
+    reajuste = salarioinicial * aumento
+    salario2 = salarioinicial + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste + reajuste
+    print("O salário será de: ", salario2)
